2b759c5020017027a01bd97a43052b2a2593658e
Iswanul Umam <umam@sepulsa.com>
2018-04-20 11:35:49 +0700

    change column

---
+++ migrations/20180420042504-create-student.js
@@ -0,0 +1,28 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Students', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Students');
+  }
+};
---
+++ models/student.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Student = sequelize.define('Student', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    date: DataTypes.STRING
+  });
+
+  
+
+  Student.associate = function(models) {
+    // associations can be defined here
+  };
+  return Student;
+};

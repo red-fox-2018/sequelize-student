b60be06943d39c5fc8b8d3e2134b4ea6f0282916
asepridwan1989 <aseprmail@gmail.com>
2018-04-20 17:19:33 +0700

    revisi model student

--- models/student.js
+++ models/student.js
@@ -13,20 +13,20 @@ module.exports = (sequelize, DataTypes) => {
             msg : `invalid email`
           },
           isUnique: function (value, next) {
-                   var self = this;
-                   User.find({where: {email: value}})
-                       .then(function (user) {
-                           // reject if a different user wants to use the same email
-                           if (user && self.id !== user.id) {
-                               return next('Email already in use!');
-                           }
-                           return next();
-                       })
-                       .catch(function (err) {
-                           return next(err);
-                       });
-            }
+            var self = this;
+            Student.findOne({where: {email: value}})
+            .then(function (student) {
+              if (student) {
+                return next('Email already in use!');
+              }else{
+                return next();
+              }
+          })
+          .catch(function (err) {
+            return next(err);
+          });
         }
+      }
     },
     phone: {
       type: DataTypes.STRING,
@@ -41,15 +41,15 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     }
-    // heigh:{
-    //   type: DataTypes.INTEGER,
-    //   validate:{
-    //     min: {
-    //       args:150,
-    //       msg: `heigh is lower than 150`
-    //    }
-    // }
-    // }
+    heigh:{
+      type: DataTypes.INTEGER,
+      validate:{
+        min: {
+          args:150,
+          msg: `heigh is lower than 150`
+       }
+      }
+    }
   }, {});
   Student.associate = function(models) {
     // associations can be defined here

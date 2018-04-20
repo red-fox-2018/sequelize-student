2354b26aae86e316247f796a019a1b8e4ffec8eb
Iswanul Umam <umam@sepulsa.com>
2018-04-20 19:18:05 +0700

    validate email

--- index.js
+++ index.js
@@ -30,23 +30,23 @@ switch(command) {
 
 // ---------------------------------------------
 
-// Student.findAll().then(rows => {
-//   console.log(` ----------get fullName()`);
-//   rows.forEach(row => {
-//     console.log(row.getFullName());
-//   })
-// });
+Student.findAll().then(rows => {
+  console.log(` ----------get fullName()`);
+  rows.forEach(row => {
+    console.log(row.getFullName());
+  })
+});
 
-// Student.findAll().then(rows => {
-//   console.log(`------------ get getAge()`);
-//   rows.forEach(row => {
-//     console.log(row.firstName, row.getAge());
-//   })
-// });
+Student.findAll().then(rows => {
+  console.log(`------------ get getAge()`);
+  rows.forEach(row => {
+    console.log(row.firstName, row.getAge());
+  })
+});
 
-// Student.getFemaleStudent().then(femaleStudents => {
-//   console.log(`------------ get getFemaleStudent()`);
-//   femaleStudents.forEach(fs => {
-//     console.log(fs.getFullName());
-//   })
-// });
+Student.getFemaleStudent().then(femaleStudents => {
+  console.log(`------------ get getFemaleStudent()`);
+  femaleStudents.forEach(fs => {
+    console.log(fs.getFullName());
+  })
+});

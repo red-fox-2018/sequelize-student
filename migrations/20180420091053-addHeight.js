afb40469523bc0a389fcaa8c997919449ccc6d5e
soni wijaya <soniwijaya59@yahoo.co.id>
2018-04-20 19:20:31 +0700

    sequelize instance class

---
+++ migrations/20180420091053-addHeight.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+    */
+    // migration:generate --name addHeight
+    return [
+      queryInterface.addColumn(
+        'Students',
+        'height',
+        {
+          type: Sequelize.INTEGER,
+        }
+      ),
+    ]
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
